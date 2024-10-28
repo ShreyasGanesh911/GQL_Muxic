506f6dfcdae6674499129baac34844cde4a1b275
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-10-28 21:28:50 +0530

    db connection

---
+++ src/db/index.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+const URI = process.env.MONGO_URI
+const connection = async()=>{
+    await mongoose.connect(URI).then(()=>console.log("connected to database!")).catch((e)=>console.log("Error in connecting to database",e))
+}
+
+export default connection
